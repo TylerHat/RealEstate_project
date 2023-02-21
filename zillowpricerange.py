1743ee79d92256688940a189392d8758eee8dde0
Tyler Hatfield <hatfield.tyler@gmail.com>
2023-02-20 21:41:05 -0500

    added mortgage payment calc plus dummy proof code

--- zillowpricerange.py
+++ zillowpricerange.py
@@ -107,17 +107,20 @@ country = find_value_after_word("country",',').strip().strip("\"")
 description_full = find_value_after_word("description",'\",').strip().strip("\"").split('\n')
 description_full = ' '.join(description_full).replace('\t', '').replace(", ",",")
 
+##use this for $/sqft 
+house_sqft = float(find_value_after_word("livingArea",',').strip().strip("\""))
+lotSize_sqft = float(find_value_after_word("lotSize",',').strip().strip("\""))
+
 
 ######## Section for Home Price Values ############
+monthly_HoaFee = float(find_value_after_word("monthlyHoaFee",',').strip().strip("\""))
+lastSoldPrice = float(find_value_after_word("lastSoldPrice",',').strip().strip("\""))
+monthly_rentZestimate = float(find_value_after_word("rentZestimate",',').strip().strip("\""))
+zestimateHouse = float(find_value_after_word("\"zestimate",',').strip().strip("\""))
 
-monthly_HoaFee = find_value_after_word("monthlyHoaFee",',').strip().strip("\"")
-lastSoldPrice = find_value_after_word("lastSoldPrice",',').strip().strip("\"")
-monthly_rentZestimate = find_value_after_word("rentZestimate",',').strip().strip("\"")
-zestimateHouse = find_value_after_word("\"zestimate",',').strip().strip("\"")
 
 
 ######## Section for Taxes ############
-
 tax_values= []
 valuIncreaseRates = []
 taxpaid = []
@@ -130,7 +133,7 @@ while i1 <=21:
     comparable_years.append(year_start_tax - i1)
     i1+=1
 else:
-    print("Year " + str(int(comparable_years[0])) + " data" )
+    print()##"Year " + str(int(comparable_years[0])) + " data" )
 
 i2=0
 while i2<=21:
@@ -140,7 +143,7 @@ while i2<=21:
     taxIncrease.append(float(find_value_after_word("taxIncreaseRate_"+str(i2+1),",")))
     i2+=1
 else:
-    print('error in array filling')
+    print('')
 
 
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
@@ -160,3 +163,68 @@ axs[1, 1].set_title('Tax Increase by Year')
 plt.savefig('my_plots.png')
 
 ##plt.show()
+
+######## Section for Analytics ############
+
+""" Mortgage payment = (P * r * (1 + r)^n) / ((1 + r)^n - 1)
+where:
+
+P is the principal (the amount of the loan)
+r is the monthly interest rate (annual interest rate divided by 12)
+n is the total number of payments (the number of years of the loan multiplied by 12)
+Note that this equation assumes a fixed-rate mortgage. If you have an adjustable-rate mortgage, the mortgage payment may change over time."""
+
+option_1 = input("Do you have the monthly interest rate or the yearly? (0 for monthly, 1 for yearly)")
+if option_1 == 0:
+    princible_rate = float(input("What is the interest rate?"))
+    if princible_rate == "":
+        princible_rate =3
+    princible_rate = princible_rate/(12/100)
+elif option_1 == 1:
+    princible_rate = float(input("What is the interest rate?"))
+    if princible_rate == "":
+        princible_rate =3
+    princible_rate = princible_rate/(100)
+else:
+    ##print("that is not a correct option")
+    princible_rate = .03
+
+
+initialpayment_Percent = int(input("How much (%) of the total value do you plan on puting down?"))/100
+princible_loan_zest = float(zestimateHouse * (1-initialpayment_Percent))
+numOfPayments = int(input("What is total amount of payments?"))
+
+##print("princible" + princible_loan_zest)
+print("princible_rate: " + str(princible_rate) +"\tinicialpayment%: " + str(initialpayment_Percent)+"\tprinc_loan_zestimate: " + str(princible_loan_zest)+ "\t# of payments: " + str(numOfPayments))
+
+mortgage_payment_zest = (princible_loan_zest * initialpayment_Percent * princible_rate * (1 + princible_rate) ** numOfPayments)/((1 + princible_rate) ** (360-1))
+
+pr(mortgage_payment_zest)
+
+
+"""
+Break-even point = Total cost of owning the property / Rental income per year
+
+
+"""
+
+breakEvenPoint = round(zestimateHouse/monthly_rentZestimate, 2)
+print("zestimateHouse: "+ str(zestimateHouse)+ "\tmonthly_rentZestimate: "+ str(monthly_rentZestimate)+ "\nBreak Even Point: "+ str(breakEvenPoint))
+
+
+
+
+
+"""
+The cap rate is found by dividing the property's net operating expenses by its purchase price. You can find the cap rate by doing the following: 
+
+Find your gross income by taking the average monthly rent for your property and multiplying it by 11.5. This will show the maximum amount you can make from the property, allowing for a two-week per year vacancy.
+
+Then, subtract your monthly operating expenses ( utilities, taxes, maintenance) from your gross income to get your net income.
+
+Divide your net income by the purchase price to find your cap rate.
+
+Multiply the cap rate by 100 to find the percentage of your potential returns on the property.
+
+
+"""
