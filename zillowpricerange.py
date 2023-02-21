0c5bc2c52f37487cc514d0cf6ac76391561b4b73
Tyler Hatfield <hatfield.tyler@gmail.com>
2023-02-21 18:41:26 -0500

    added predicting 4 taxes and convert values 2 html

--- zillowpricerange.py
+++ zillowpricerange.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
-import array as arr
-import numpy
+import pandas as pd
+import webbrowser
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
 
 file = open('housedata.txt', 'r')
 read = file.readlines()
@@ -46,14 +49,15 @@ text_file.close()
 
 
 
-def correction_value_error(filename, word):
+def correction_value_error(filename, word, newword):
     with open(filename, 'r') as file:
         contents = file.read()
-    contents = contents.replace(word, "valuIncreaseRate")
+    contents = contents.replace(word, newword)
     with open(filename, 'w') as file:
         file.write(contents)
 
-correction_value_error("housedata_tabed.txt", "valueIncreaseRate")
+correction_value_error("housedata_tabed.txt", "valueIncreaseRate", "valuIncreaseRate")
+correction_value_error("housedata_tabed.txt", '"zestimate":', '"HouseZestimate":')
 
 def increment_word_count(filename, word):
 
@@ -101,11 +105,15 @@ def find_value_after_word(word, endchar):
 city_address = find_value_after_word("city",',').strip().strip("\"")
 state_address =  find_value_after_word("state",',').strip().strip("\"")
 streetAddress =  find_value_after_word("streetAddress",',').strip().strip("\"")
-zipCode =  find_value_after_word("zipcode",',').strip().strip("\"")
+zipCode =  find_value_after_word("zipcode",',').strip().strip("\"\}")
 county = find_value_after_word("county",',').strip().strip("\"")
 country = find_value_after_word("country",',').strip().strip("\"")
 description_full = find_value_after_word("description",'\",').strip().strip("\"").split('\n')
 description_full = ' '.join(description_full).replace('\t', '').replace(", ",",")
+
+full_address = streetAddress + ", " + city_address + ", " + county +" "+ zipCode +" " + country
+
+
 
 ##use this for $/sqft 
 house_sqft = float(find_value_after_word("livingArea",',').strip().strip("\""))
@@ -113,10 +121,12 @@ lotSize_sqft = float(find_value_after_word("lotSize",',').strip().strip("\""))
 
 
 ######## Section for Home Price Values ############
-monthly_HoaFee = float(find_value_after_word("monthlyHoaFee",',').strip().strip("\""))
+monthly_HoaFee = float(find_value_after_word("monthlyHoaFee",','))
 lastSoldPrice = float(find_value_after_word("lastSoldPrice",',').strip().strip("\""))
 monthly_rentZestimate = float(find_value_after_word("rentZestimate",',').strip().strip("\""))
-zestimateHouse = float(find_value_after_word("\"zestimate",',').strip().strip("\""))
+zestimateHouse = float(find_value_after_word("HouseZestimate",','))
+
+print(zestimateHouse)
 
 
 
@@ -133,7 +143,7 @@ while i1 <=21:
     comparable_years.append(year_start_tax - i1)
     i1+=1
 else:
-    print()##"Year " + str(int(comparable_years[0])) + " data" )
+    print("")
 
 i2=0
 while i2<=21:
@@ -145,8 +155,33 @@ while i2<=21:
 else:
     print('')
 
+def predic_value(past_array, amount_to_predict):
+    # Example input data
+    data = np.array(past_array)
 
-plt.rcParams["figure.figsize"] = [7.50, 3.50]
+    # Reshape the data into a 2D array with one column
+    X = data.reshape(-1, 1)
+
+    # Use the first 5 values as training data
+    X_train = X[:5]
+    y_train = data[:5]
+
+    # Fit a linear regression model to the training data
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Predict the next 5 values
+    X_test = X[amount_to_predict:]
+    y_pred = model.predict(X_test)
+
+    # Print the predicted values
+    print("Predicted values:", y_pred)
+    return y_pred
+
+    
+
+
+plt.rcParams["figure.figsize"] = [15.50, 9.50]
 plt.rcParams["figure.autolayout"] = True
 
 ##Subplots for Tax Graphs
@@ -174,33 +209,31 @@ r is the monthly interest rate (annual interest rate divided by 12)
 n is the total number of payments (the number of years of the loan multiplied by 12)
 Note that this equation assumes a fixed-rate mortgage. If you have an adjustable-rate mortgage, the mortgage payment may change over time."""
 
-option_1 = input("Do you have the monthly interest rate or the yearly? (0 for monthly, 1 for yearly)")
+option_1 = input("Do you have the monthly interest rate or the yearly? (0 for monthly, 1 for yearly)") or 1
 if option_1 == 0:
-    princible_rate = float(input("What is the interest rate?"))
-    if princible_rate == "":
-        princible_rate =3
+    princible_rate = float(input("What is the interest rate?") or 6)
     princible_rate = princible_rate/(12/100)
 elif option_1 == 1:
-    princible_rate = float(input("What is the interest rate?"))
-    if princible_rate == "":
-        princible_rate =3
+    princible_rate = float(input("What is the interest rate?") or 6)
     princible_rate = princible_rate/(100)
 else:
-    ##print("that is not a correct option")
-    princible_rate = .03
+    print("that is not a correct option")
 
 
-initialpayment_Percent = int(input("How much (%) of the total value do you plan on puting down?"))/100
-princible_loan_zest = float(zestimateHouse * (1-initialpayment_Percent))
-numOfPayments = int(input("What is total amount of payments?"))
+
+initialpayment_Percent = int(input("How much (%) of the total value do you plan on puting down?") or 20)/100 
+princible_loan_zest = float(zestimateHouse) * (1-initialpayment_Percent) 
+numOfPayments = int(input("What is total amount of payments?") or 360) 
 
 ##print("princible" + princible_loan_zest)
-print("princible_rate: " + str(princible_rate) +"\tinicialpayment%: " + str(initialpayment_Percent)+"\tprinc_loan_zestimate: " + str(princible_loan_zest)+ "\t# of payments: " + str(numOfPayments))
+print("princible_rate: " + str(princible_rate) +"\tinicialpayment%: " + str(initialpayment_Percent)+"\princible_loan_zest: " + str(princible_loan_zest)+ "\t# of payments: " + str(numOfPayments))
 
 mortgage_payment_zest = (princible_loan_zest * initialpayment_Percent * princible_rate * (1 + princible_rate) ** numOfPayments)/((1 + princible_rate) ** (360-1))
 
 pr(mortgage_payment_zest)
 
+
+##html_house_details=f"<h1>My value is {my_value}</h1>"
 
 """
 Break-even point = Total cost of owning the property / Rental income per year
@@ -227,4 +260,93 @@ Divide your net income by the purchase price to find your cap rate.
 Multiply the cap rate by 100 to find the percentage of your potential returns on the property.
 
 
+
+
+
 """
+
+html_header_info_1 = "This report was run for: "+full_address 
+html_header_info_2 = description_full
+html_header_info_3 =" Additional calculations:\n" +" Princible Interest Rate: " + \
+    str(princible_rate) +" Initial Loan Payment: " + str(initialpayment_Percent)+" Princible Loan Amount for Zestimate: " + str(princible_loan_zest) + \
+        " # of payments: " + str(numOfPayments)
+html_header_info_4 = " Breakeven Point: "+ str(breakEvenPoint) +" Morgage Payment bases on Zesimate: "+ str(mortgage_payment_zest)
+"""
+
+Section for table of PRoperty calculator
+
+"""
+
+house_prop_data = []
+expenses_per_month=200
+
+total_rent_income = monthly_rentZestimate *12
+total_expenses = expenses_per_month *12
+net_income = total_rent_income-total_expenses
+
+##accumeulated values
+accumulated_total_income = total_rent_income
+accumulated_total_expense = total_expenses
+accumulated_net_income = net_income
+
+total_roi = ((zestimateHouse+ princible_loan_zest) +net_income)/(zestimateHouse+ princible_loan_zest)
+
+house_prop_data.append({
+        "Year": int(year_start_tax),
+        "Total Income": round(total_rent_income, 3),
+        "Total Expenses": round(total_expenses, 3),
+        "Net Income": round(net_income, 3),
+        "Total ROI": round(total_roi, 3),
+        "Acc. Total Income": round(accumulated_total_income, 3),
+        "Acc. Total Expence": round(accumulated_total_expense, 3),
+        "Acc. Total Net Income": round(accumulated_net_income, 3)
+    })
+
+
+for year in range(int(year_start_tax+1),(int(year_start_tax)+20)):
+
+    total_rent_income *= 1.03
+    total_expenses *= 1.03
+    net_income *= 1.03
+
+    total_roi = ((zestimateHouse+ princible_loan_zest) +net_income)/(zestimateHouse+ princible_loan_zest)
+
+    accumulated_total_income += total_rent_income
+    accumulated_total_expense += total_expenses
+    accumulated_net_income += net_income
+
+    house_prop_data.append({
+            "Year": int(year),
+            "Total Income": round(total_rent_income, 3),
+            "Total Expenses": round(total_expenses, 3),
+            "Net Income": round(net_income, 3),
+            "Total ROI": round(total_roi, 3),
+            "Acc. Total Income": round(accumulated_total_income, 3),
+            "Acc. Total Expence": round(accumulated_total_expense, 3),
+            "Acc. Total Net Income": round(accumulated_net_income, 3)
+        })
+df = pd.DataFrame(house_prop_data)
+
+html_yr_calc_tb = df.to_html(classes='table table-stripped')
+  
+df.to_csv('house30yrinvestment.csv')
+
+
+# Load the PNG image file
+img_path = 'my_plots.png'
+image_tag = f'<img src="{img_path}" alt="image" width="1200">'
+
+# Convert the Pandas DataFrame to an HTML table
+table_html = df.to_html(index=False)
+
+# Concatenate the image and table HTML tags
+full_html = f'{html_header_info_1}<br>{html_header_info_2}<br>{html_header_info_3}<br>{html_header_info_4}<br>{image_tag}<br>{table_html}'
+
+# Save the HTML code to a file
+with open('result.html', 'w') as f:
+    f.write(full_html)
+
+
+
+# open the HTML page in a new tab in the default web browser
+webbrowser.open_new_tab("result.html")
